32fddac2eb989b8c6e3749cd76586d0bb5da2b6a
Karel Malý <105454484+karlosmatos@users.noreply.github.com>
2024-04-01 19:44:04 +0200

    Refactor code to improve performance and readability

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,118 +7,56 @@ import DropDown, { VibeType } from '../components/DropDown';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import LoadingDots from '../components/LoadingDots';
-import {
-  createParser,
-  ParsedEvent,
-  ReconnectInterval,
-} from 'eventsource-parser';
 import Toggle from '../components/Toggle';
 
 const Home: NextPage = () => {
   const [loading, setLoading] = useState(false);
-  const [bio, setBio] = useState('');
+  const [property, setProperty] = useState('');
   const [vibe, setVibe] = useState<VibeType>('Professional');
-  const [generatedBios, setGeneratedBios] = useState<String>('');
+  const [generatedProperties, setGeneratedProperties] = useState<any[]>([]);
   const [isGPT, setIsGPT] = useState(false);
 
-  const bioRef = useRef<null | HTMLDivElement>(null);
+  const propertyRef = useRef<null | HTMLDivElement>(null);
 
-  const scrollToBios = () => {
-    if (bioRef.current !== null) {
-      bioRef.current.scrollIntoView({ behavior: 'smooth' });
+  const scrollToProperties = () => {
+    if (propertyRef.current !== null) {
+      propertyRef.current.scrollIntoView({ behavior: 'smooth' });
     }
   };
 
-  const prompt = `Generate 3 ${
-    vibe === 'Casual' ? 'relaxed' : vibe === 'Funny' ? 'silly' : 'Professional'
-  } twitter biographies with no hashtags and clearly labeled "1.", "2.", and "3.". Only return these 3 twitter bios, nothing else. ${
-    vibe === 'Funny' ? 'Make the biographies humerous' : ''
-  }Make sure each generated biography is less than 300 characters, has short sentences that are found in Twitter bios, and feel free to use this context as well: ${bio}${
-    bio.slice(-1) === '.' ? '' : '.'
-  }`;
-
-  console.log({ prompt });
-  console.log({ generatedBios });
-
-  const generateBio = async (e: any) => {
+  const generateProperty = async (e: any) => {
     e.preventDefault();
-    setGeneratedBios('');
+    setGeneratedProperties([]);
     setLoading(true);
-    const response = await fetch(isGPT ? '/api/openai' : '/api/mistral', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        prompt,
-      }),
+  
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/real_estate`, {
+      method: 'GET',
     });
-
+  
     if (!response.ok) {
       throw new Error(response.statusText);
     }
-
-    // This data is a ReadableStream
-    const data = response.body;
-    if (!data) {
-      return;
-    }
-
-    const onParseGPT = (event: ParsedEvent | ReconnectInterval) => {
-      if (event.type === 'event') {
-        const data = event.data;
-        try {
-          const text = JSON.parse(data).text ?? '';
-          setGeneratedBios((prev) => prev + text);
-        } catch (e) {
-          console.error(e);
-        }
-      }
-    };
-
-    const onParseMistral = (event: ParsedEvent | ReconnectInterval) => {
-      if (event.type === 'event') {
-        const data = event.data;
-        try {
-          const text = JSON.parse(data).choices[0].text ?? '';
-          setGeneratedBios((prev) => prev + text);
-        } catch (e) {
-          console.error(e);
-        }
-      }
-    };
-
-    const onParse = isGPT ? onParseGPT : onParseMistral;
-
-    // https://web.dev/streams/#the-getreader-and-read-methods
-    const reader = data.getReader();
-    const decoder = new TextDecoder();
-    const parser = createParser(onParse);
-    let done = false;
-    while (!done) {
-      const { value, done: doneReading } = await reader.read();
-      done = doneReading;
-      const chunkValue = decoder.decode(value);
-      parser.feed(chunkValue);
-    }
-    scrollToBios();
+  
+    const data = await response.json();
+    setGeneratedProperties(data);
+    scrollToProperties();
     setLoading(false);
   };
 
   return (
     <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Head>
-        <title>Twitter Bio Generator</title>
+        <title>Your AI Home Advisor</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
         <p className="border rounded-2xl py-1 px-4 text-slate-500 text-sm mb-5 hover:scale-105 transition duration-300 ease-in-out">
-          <b>96,434</b> bios generated so far
+          <b>96,434</b> happy users and counting
         </p>
         <h1 className="sm:text-6xl text-4xl max-w-[708px] font-bold text-slate-900">
-          Generate your next Twitter bio using AI
+          Get your new home advise based on your life situation
         </h1>
         <div className="mt-7">
           <Toggle isGPT={isGPT} setIsGPT={setIsGPT} />
@@ -134,16 +72,16 @@ const Home: NextPage = () => {
               className="mb-5 sm:mb-0"
             />
             <p className="text-left font-medium">
-              Drop in your job{' '}
-              <span className="text-slate-500">(or your favorite hobby)</span>.
+              Drop in your current life situation{' '}
+              <span className="text-slate-500">(or your expectation)</span>.
             </p>
           </div>
           <textarea
-            value={bio}
-            onChange={(e) => setBio(e.target.value)}
+            value={property}
+            onChange={(e) => setProperty(e.target.value)}
             rows={4}
             className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
-            placeholder={'e.g. Amazon CEO'}
+            placeholder={'e.g. 25 years old, single, working as a software engineer, looking for a new home in Prague.'}
           />
           <div className="flex mb-5 items-center space-x-3">
             <Image src="/2-black.png" width={30} height={30} alt="1 icon" />
@@ -156,9 +94,9 @@ const Home: NextPage = () => {
           {!loading && (
             <button
               className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
-              onClick={(e) => generateBio(e)}
+              onClick={(e) => generateProperty(e)}
             >
-              Generate your bio &rarr;
+              Get your dream home &rarr;
             </button>
           )}
           {loading && (
@@ -177,36 +115,38 @@ const Home: NextPage = () => {
         />
         <hr className="h-px bg-gray-700 border-1 dark:bg-gray-700" />
         <div className="space-y-10 my-10">
-          {generatedBios && (
+          {generatedProperties && (
             <>
               <div>
                 <h2
                   className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto"
-                  ref={bioRef}
+                  ref={propertyRef}
                 >
-                  Your generated bios
+                  Real Estate Properties
                 </h2>
               </div>
-              <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
-                {generatedBios
-                  .substring(generatedBios.indexOf('1') + 3)
-                  .split(/2\.|3\./)
-                  .map((generatedBio) => {
-                    return (
-                      <div
-                        className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
-                        onClick={() => {
-                          navigator.clipboard.writeText(generatedBio);
-                          toast('Bio copied to clipboard', {
-                            icon: '✂️',
-                          });
-                        }}
-                        key={generatedBio}
-                      >
-                        <p>{generatedBio}</p>
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                {generatedProperties.map((property: any) => (
+                  <div
+                    className="bg-white rounded-xl shadow-md overflow-hidden"
+                    key={property._id.$oid}
+                  >
+                    <div className="relative">
+                      <img
+                        src={property._links.images[0].href}
+                        alt={property.name}
+                        className="w-full h-48 object-cover"
+                      />
+                      <div className="absolute top-0 right-0 bg-black text-white px-2 py-1 m-2 rounded">
+                        {property.price_czk.value_raw} {property.price_czk.unit}
                       </div>
-                    );
-                  })}
+                    </div>
+                    <div className="p-4">
+                      <h3 className="text-xl font-semibold">{property.name}</h3>
+                      <p className="text-gray-500">{property.locality}</p>
+                    </div>
+                  </div>
+                ))}
               </div>
             </>
           )}
